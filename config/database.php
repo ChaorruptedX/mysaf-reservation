a5d4ba80070aa3f3d7f1cdbfa6b765241301219e
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-03 15:51:52 +0800

    Add PHP Database Connection using PDO (PHP Data Objects)

--- config/database.php
+++ config/database.php
@@ -1,3 +1,20 @@
 <?php
 
+    $servername = "localhost";
+    $databasename = "mysaf_reservation";
+    $username = "root";
+    $password = "";
+
+    try
+    {
+        $conn = new PDO("mysql:host=" . $servername . ";dbname=" . $databasename, $username, $password); // Create Connection
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set the PDO Error Mode to Exception
+
+        // dd("Connected successfully");
+    }
+    catch (PDOException $e)
+    {
+        dd("Connection failed: " . $e->getMessage());
+    }
+
 ?>
