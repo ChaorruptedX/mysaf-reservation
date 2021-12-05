bf29e37b3cbca1ce400b3c12832bcc710cc88a27
Asyraf391 <asyrafhanif391@gmail.com>
2021-12-05 15:14:31 +0800

    Login Page v2

--- public/login.php
+++ public/login.php
@@ -1,23 +1,31 @@
 <?php require_once ('layouts\header.php'); ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<body>
-    <div class="background">
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <h1> MySaf Reservation </h1>
+      <div>
+        <img src ="<?= constant("BASEURL") . 'assets/image/logo.png' ?>" id="icon" alt="System Icon" />
+      <div>
+  
+    <h2 class="login active"> Sign In </h2>
+    <h2 class="login inactive underlineHover">Sign Up </h2>
+
+    <!-- Login Form -->
     <form>
-        <h3>Login Here</h3>
-        <h4><a href="https://www.w3schools.com/">New user? Click here to register.</a></h4>
-
-        <label for="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" required>
-
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" required>
-
-        <button>Log In</button>
+      <input type="text" id="login" class="input login" name="login" placeholder="Email/Phone number">
+      <input type="password" id="password" class="input login" name="login" placeholder="password">
+      <button class="button login">LOG IN</button>
     </form>
-    </div>
-</body>
-</html>
+
+    <!-- Remind Passowrd -->
+    <!--div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div-->
+
+  </div>
+</div>
 
 <?php require_once ('layouts\footer.php'); ?>
