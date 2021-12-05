08de8fd953cf5fef3044c18e5b7b58fcbf809424
Asyraf391 <asyrafhanif391@gmail.com>
2021-12-05 16:04:30 +0800

    Login Page V3

--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -1,5 +1,5 @@
 </div>
-
+<br> <br>
 <footer>
     &copy; <?= date('Y'); ?> MySaf Reservation
 </footer>
--- public/login.php
+++ public/login.php
@@ -10,14 +10,23 @@
         <img src ="<?= constant("BASEURL") . 'assets/image/logo.png' ?>" id="icon" alt="System Icon" />
       <div>
   
-    <h2 class="login active"> Sign In </h2>
-    <h2 class="login inactive underlineHover">Sign Up </h2>
+    <h2 class="login active" id="sign-in"> Sign In </h2>
+    <h2 class="login inactive underlineHover" id="sign-up">Sign Up </h2>
 
     <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="input login" name="login" placeholder="Email/Phone number">
-      <input type="password" id="password" class="input login" name="login" placeholder="password">
+    <form id="signin" >
+      <input type="text" id="email-login" class="input login" name="email" placeholder="Email">
+      <input type="password" id="password-login" class="input login" name="password" placeholder="password">
       <button class="button login">LOG IN</button>
+    </form>
+
+    <!-- Sign up Form -->
+    <form id="signup" style="display:none">
+      <input type="email" id="email" class="input login" name="email" placeholder="Email">
+      <input type="password" id="password-signup" class="input login" name="password" placeholder="Password">
+      <input type="text" id="name" class="input login" name="name" placeholder="Name">
+      <input type="number" id="phone" class="input login" name="phone" placeholder="Phone Number">
+      <button class="button login">Register</button>
     </form>
 
     <!-- Remind Passowrd -->
@@ -27,5 +36,28 @@
 
   </div>
 </div>
+<script type="text/javascript">
+$(document).ready(function()
+  {
+    $(document).on("click", "h2#sign-in", function()  // Click Sign in tab
+    {
+      $("form#signin").show();
+      $("form#signup").hide();
+
+      $( "h2#sign-up" ).removeClass( "active" ).addClass( "inactive underlineHover" );
+      $( "h2#sign-in" ).removeClass( "inactive underlineHover" ).addClass( "active" );
+      
+    });
+
+    $(document).on("click","h2#sign-up", function() // Click Sign up tab
+    {
+      $("form#signup").show();
+      $("form#signin").hide();
+
+      $( "h2#sign-up" ).removeClass( "inactive underlineHover" ).addClass( "active" );
+      $( "h2#sign-in" ).removeClass( "active" ).addClass( "inactive underlineHover" );
+    });
+  });
+</script>
 
 <?php require_once ('layouts\footer.php'); ?>
