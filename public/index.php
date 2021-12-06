4d6a3a131d036f74a969a2a2f454fe12d2141ccd
Asyraf391 <asyrafhanif391@gmail.com>
2021-12-06 15:11:59 +0800

    Home Page Interface

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php require_once ('layouts\header.php'); guestSession(); ?>
 
-<p>Hello World!</p>
+<style>
+h1.home{
+    text-align: center;
+}
+.content.home {
+    margin: auto;
+    width: 60%;
+    border: 3px solid #73AD21;
+    padding: 10px;
+    border-radius: 25px;
+    text-align: center;
+    background:#8FBC8F ;
+  }
+</style>
+
+<h1 class="h1 home">MASJID SAYYIDINA ABU BAKAR, UTEM</h1>
+
+<div class="content home">
+    <h2> Announcement </h2>
+    <p>Selamat Datang !</p>
+  </div>
 
 <?php require_once ('layouts\footer.php'); ?>
