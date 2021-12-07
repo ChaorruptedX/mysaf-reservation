f06a4b4003626f62d4cfcb213f68dbaa53b5c824
Asyraf391 <asyrafhanif391@gmail.com>
2021-12-07 15:57:19 +0800

    Home page updated

--- public/index.php
+++ public/index.php
@@ -18,8 +18,29 @@ h1.home{
 <h1 class="h1 home">MASJID SAYYIDINA ABU BAKAR, UTEM</h1>
 
 <div class="content home">
-    <h2> Announcement </h2>
-    <p>Selamat Datang !</p>
-  </div>
+  <h2> Announcement </h2>
+  <p>Hanya jemaah yang lengkap 2 dos vaksinasi dibenarkan solat berjemaah di masjid.</p>
+</div>
+<br>
+
+<div class="content home">
+  <h2> Friday Prayers saf Reservation</h2>
+  <table id="user">
+  <tr>
+    <td colspan="2"><h3>10 December 2021</h3></td>
+  </tr>
+    <tr>
+      <th>Open</th>
+      <th>Close</th>
+  </tr>
+
+    <tr>
+      <td>Thursday : 10:00 AM</td>
+      <td>Friday : 10:00 AM</td>
+    </tr>
+</table>
+<br>
+<a href="<?= constant("BASEURL") . 'public/login.php' ?>"><button class="button home-reserve">Reserve Now</button></a>
+</div>
 
 <?php require_once ('layouts\footer.php'); ?>
