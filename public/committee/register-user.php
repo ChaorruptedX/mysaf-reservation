01cb35f1ad10c6e12a4098468c2a6fe20d63ae29
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-07 16:54:00 +0800

    Register User Page - Add jQuery Validation

--- public/committee/register-user.php
+++ public/committee/register-user.php
@@ -7,18 +7,19 @@
 </div>
 
 <div class="wrapper form">
-    <form>
+    <form id="register-user">
         <label class="form" for="name">Name</label>
-        <input class="input form" type="text" id="name" name="name" placeholder="Enter Name">
+        <input class="input form" type="text" id="name" name="name" placeholder="Enter Name" required>
+        
+        <label class="form" for="email">Email Address</label>
+        <input class="input form" type="email" id="email" name="email" placeholder="Enter Email" required>
 
-        <label  class="form" for="email">Email Address</label>
-        <input  class="input form" type="email" id="email" name="email" placeholder="Enter Email">
+        <label class="form" for="phone">Phone Number</label>
+        <input class="input form" type="text" id="phone" name="phone" placeholder="Enter Phone Number" required>
 
-        <label  class="form" for="phone">Phone Number</label>
-        <input  class="input form" type="text" id="phone" name="phone" placeholder="Enter Phone Number">
-
-        <label  class="form" for="roles">Roles</label>
-        <select id="roles" name="roles">
+        <label class="form" for="roles">Roles</label>
+        <select id="roles" name="roles" required>
+            <option value="">Please Select ...</option>
             <option value="admin">System Admin</option>
             <option value="ajk">Mosque Committee</option>
         </select>
@@ -26,5 +27,38 @@
         <button class="button form submit">Register</button>
   </form>
 </div>
+
+<script type="text/javascript">
+
+$(function() { // Shorthand for $( document ).ready()
+
+    $("form#register-user").validate({ // Sign In Validation
+        errorElement: "div",
+        errorClass: "validation-error form-validation",
+        rules: {
+            phone: {
+                number: true,
+            }
+        },
+        messages: {
+            name: {
+                required: "Name is required",
+            },
+            email: {
+                required: "Email Address is required",
+                email: "Email address must be in the format of name@domain.com"
+            },
+            phone: {
+                required: "Phone Number is required",
+            },
+            roles: {
+                required: "User Role is required",
+            },
+        },
+    });
+
+});
+
+</script>
 
 <?php require_once ('../layouts/footer.php'); ?>
