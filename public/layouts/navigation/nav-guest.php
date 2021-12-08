da14c6de85f1e808fad5de18de8f0c14af61d05f
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-08 10:02:07 +0800

    Apply Active URL Function for Nav Guest

--- public/layouts/navigation/nav-guest.php
+++ public/layouts/navigation/nav-guest.php
@@ -1,9 +1,9 @@
 <nav class="topnav">
     <div class="container">
-        <a class="active" href="<?= constant("BASEURL") . 'public/index.php' ?>">Home</a>
+        <a class="<?= active_url("index.php"); ?>" href="<?= constant("BASEURL") . 'public/index.php' ?>">Home</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
-        <a href="<?= constant("BASEURL") . 'public/login.php' ?>" style="float: right;">Login</a>
-        <a href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">User Management</a>
+        <a class="<?= active_url("login.php"); ?>" href="<?= constant("BASEURL") . 'public/login.php' ?>" style="float: right;">Login</a>
+        <a class="<?= active_url("user-management.php"); ?>" href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">User Management</a>
     </div>
 </nav>
