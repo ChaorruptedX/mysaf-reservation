7ee6c6fc10224c479a6f7641fa2d033f064d77d1
Abbyblacky12 <95325877+Abbyblacky12@users.noreply.github.com>
2021-12-15 12:04:37 +0800

    Update reservation.php

--- public/user/reservation.php
+++ public/user/reservation.php
@@ -3,9 +3,8 @@
 <link rel ="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/reservation.css' ?>">
 
 <div class = "wrapper">
-
     <div class ="center content">
-        
+          <h2 name = "title"> Reservation Page </h2>
         <div class = "row">
             <div class = "column">
                 <img src="<?= constant("BASEURL") . 'assets/image/bground1.jpg' ?>" style="width:100%">
@@ -13,7 +12,6 @@
         </div>
         <div class = "column">
             <form action="">
-
                 <h2>Create Reservation</h2>
 
                 <h3>Select Date :</h3>
@@ -26,12 +24,10 @@
                 <input type="date" id="date_open" name="date_open"> to
                 <input type="date" id="date_close" name="date_close">
 
-                <p><input type = "reset" name = "Cancel"> <input type = "submit" name = "SAVE"></p>
-                
+                <p><input type = "reset" name = "Cancel"> <input type = "submit" name = "SAVE"></p>                
             </form>
         </div>
-    </div>
-    
+    </div> 
 </div>
 
 <?php require_once ('../layouts/footer.php'); ?>
