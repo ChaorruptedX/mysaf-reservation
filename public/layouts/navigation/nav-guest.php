65e3675186c88ae4586438c491d4a74b7039a25f
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-15 13:29:10 +0800

    Nav Guest - Update Active Link for Home

--- public/layouts/navigation/nav-guest.php
+++ public/layouts/navigation/nav-guest.php
@@ -1,6 +1,6 @@
 <nav class="topnav">
     <div class="container">
-        <a class="<?= active_url(["mysaf-reservation", "index.php"]); ?>" href="<?= constant("BASEURL") . 'public/index.php' ?>">Home</a>
+        <a class="<?= active_url(["b031910250", "mysaf-reservation", "public", "index.php"]); ?>" href="<?= constant("BASEURL") . 'public/index.php' ?>">Home</a>
         <a class="<?= active_url("contactus.php"); ?>" href="<?= constant("BASEURL") . 'public/contactus.php' ?>">Contact</a>
         <a class="<?= active_url("aboutus.php"); ?>" href="<?= constant("BASEURL") . 'public/aboutus.php' ?>">About</a>
         <a class="<?= active_url("login.php"); ?>" href="<?= constant("BASEURL") . 'public/login.php' ?>" style="float: right;">Login</a>
