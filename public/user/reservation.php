709ccfb161c49df8d90c1595d2674afd4f4578ef
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-15 11:38:00 +0800

    Update Reservation

--- public/user/reservation.php
+++ public/user/reservation.php
@@ -1,32 +1,37 @@
-<?php require_once ('../layouts/header.php'); userSession(); ?>
+<?php require_once ('../layouts/header.php'); // userSession(); ?>
 
 <link rel ="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/reservation.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/main.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/nav.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/login.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/table-style.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/form.css' ?>">
+
 <div class = "wrapper">
-    <img class="bground" src="assets/image/test2.jpg">
+
     <div class ="center content">
         
         <div class = "row">
             <div class = "column">
-                <img src="assets/image/bground.jpg" style="width:100%">
+                <img src="<?= constant("BASEURL") . 'assets/image/bground1.jpg' ?>" style="width:100%">
             </div>
+        </div>
         <div class = "column">
             <form action="">
+
                 <h2>Create Reservation</h2>
+
                 <h3>Select Date :</h3>
                 <p><input type="date" id="date" name="date"></p>
+
                 <h3>Capacity limit </h3>
                 <p><input type="number" id="capacity" name="capacity"></p>
+
                 <h3>Opening from :</h3>
                 <input type="date" id="date_open" name="date_open"> to
                 <input type="date" id="date_close" name="date_close">
+
                 <p><input type = "reset" name = "Cancel"> <input type = "submit" name = "SAVE"></p>
+                
             </form>
         </div>
     </div>
+    
 </div>
+
 <?php require_once ('../layouts/footer.php'); ?>
