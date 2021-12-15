6d6fb2657693226f5cf86e32280faceba5eb85d8
ChaorruptedX <ChaorruptedX@gmail.com>
2021-12-15 11:38:29 +0800

    Update Guest Navigation

--- public/layouts/navigation/nav-guest.php
+++ public/layouts/navigation/nav-guest.php
@@ -1,9 +1,12 @@
 <nav class="topnav">
     <div class="container">
         <a class="<?= active_url("index.php"); ?>" href="<?= constant("BASEURL") . 'public/index.php' ?>">Home</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a class="<?= active_url("contactus.php"); ?>" href="<?= constant("BASEURL") . 'public/contactus.php' ?>">Contact</a>
+        <a class="<?= active_url("aboutus.php"); ?>" href="<?= constant("BASEURL") . 'public/aboutus.php' ?>">About</a>
         <a class="<?= active_url("login.php"); ?>" href="<?= constant("BASEURL") . 'public/login.php' ?>" style="float: right;">Login</a>
+
+        <!-- Temporary Link for Assignment 2 Presentation Purpose -->
         <a class="<?= active_url(["user-management.php", "register-user.php"]); ?>" href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">User Management</a>
+        <a class="<?= active_url("reservation.php"); ?>" href="<?= constant("BASEURL") . 'public/user/reservation.php' ?>">Reservation</a>
     </div>
 </nav>
