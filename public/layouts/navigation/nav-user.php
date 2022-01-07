a5eefc8dbfc0f35dd95736671adcba46920865cc
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-07 22:05:19 +0800

    Remove Create Reservation for User

--- public/layouts/navigation/nav-user.php
+++ public/layouts/navigation/nav-user.php
@@ -1,8 +1,8 @@
 <nav class="topnav">
     <div class="container">
         <a class="<?= active_url(["b031910250", "mysaf-reservation", "public", "user", "index.php"]); ?>" href="<?= constant("BASEURL") . 'public/user/index.php' ?>">Home</a>
-        <a class="<?= active_url("create-reservation.php"); ?>" href="<?= constant("BASEURL") . 'public/user/create-reservation.php' ?>">Reservation</a>
-        <a href="#report">Report</a>
+        <a href="#my-reservation">My Reservation</a>
+        <a href="#history">History</a>
         <a href="#my-account">My Account</a>
         <a class="<?= active_url("logout.php"); ?>" href="<?= constant("BASEURL") . 'public/logout.php' ?>" style="float: right;">Logout</a>
     </div>
