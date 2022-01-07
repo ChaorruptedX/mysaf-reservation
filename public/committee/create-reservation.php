5adb41a20050ca090ef2f96076930ebf884f4098
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-07 22:07:07 +0800

    Add Create Reservation for Committee

---
+++ public/committee/create-reservation.php
@@ -0,0 +1,35 @@
+<?php require_once ('../layouts/header.php'); userSession(); ?>
+
+<div>
+    <br>
+    <h1>Create Saf Reservation</h1>
+</div>
+
+<link rel ="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/reservation.css' ?>">
+
+<div class="wrapper form">
+    <form>
+        <label class="form" for="date">Prayer Date</label>
+        <input class="input form" type="date" name="date">
+        
+        <label class="form" for="capacity">Set Capcity</label>
+        <select name="capacity">
+            <option value="50">50</option>
+            <option value="100">100</option>
+            <option value="150">150</option>
+            <option value="200">200</option>
+        </select>
+
+        <label class="form" for="phone">Reservation Open Day&Date</label>
+        <input class="input form" type="date" name="open_date"> 
+        <input class="input form" type="time" name="open_time"> 
+
+        <label class="form" for="phone">Reservation Close Day&Date</label>
+        <input class="input form" type="date" name="close_date"> 
+        <input class="input form" type="time" name="close_time"> 
+
+        <button class="form submit">Create Reservation</button>
+  </form>
+</div>
+<br>
+<?php require_once ('../layouts/footer.php'); ?>
--- public/layouts/navigation/nav-committee.php
+++ public/layouts/navigation/nav-committee.php
@@ -1,7 +1,8 @@
 <nav class="sidenav">
     <a class="<?= active_url(["b031910250", "mysaf-reservation", "public", "committee", "index.php"]); ?>" href="<?= constant("BASEURL") . 'public/committee/index.php' ?>">Home</a>
     <a class="<?= active_url(["user-management.php", "register-user.php"]); ?>" href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">User Management</a>
-    <a href="#manage-reservation">Manage Reservation</a>
+    <a class="<?= active_url("create-reservation.php"); ?>" href="<?= constant("BASEURL") . 'public/committee/create-reservation.php' ?>">User Management</a>
+    <a href="#list-of-reservation">List of Reservation</a>
     <a href="#my-account">My Account</a>
     <a class="<?= active_url("logout.php"); ?>" href="<?= constant("BASEURL") . 'public/logout.php' ?>">Logout</a>
 </nav>
