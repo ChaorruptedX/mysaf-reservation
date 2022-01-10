9304094e42c37fd8df69ca2137ed12b3f008f7a2
Asyraf391 <asyrafhanif391@gmail.com>
2022-01-10 16:10:56 +0800

    [Function] - View User Admin and Committee

--- public/committee/user-management.php
+++ public/committee/user-management.php
@@ -1,5 +1,40 @@
 <?php require_once ('../layouts/header.php'); committeeSession(); ?>
 
+<?php 
+
+  try 
+  {
+    $stmt = $conn->prepare("
+    SELECT
+      personal_detail.id,
+      personal_detail.name,
+      personal_detail.tel_no,
+      user.id_role AS ROLE,
+      lookup_role.description AS role_desc
+    FROM personal_detail
+      LEFT JOIN user
+        ON personal_detail.id_user = user.id
+      LEFT JOIN lookup_role
+        ON user.id_role = lookup_role.id
+      WHERE
+        personal_detail.deleted_at='0'
+    ");
+
+    $stmt->execute();
+
+    // Set the Resulting Array to Associative
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $row = $stmt->fetchAll();
+
+    
+  }
+ catch (PDOException $e)
+  {
+    dd("Error: " . $e->getMessage());
+  }
+?>
+
 <div>
     <br>
     <h1>List of Users</h1>
@@ -9,37 +44,31 @@
 <table id="user">
 <br>
   <tr>
-    <th>No.</th>
+    <th>ID</th>
     <th>Name</th>
     <th>Phone Number</th>
     <th>Roles</th>
-    <th>Actions</th>
+    <th>Action</th>
+  
   </tr>
+
+  <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+  <?php   // LOOP TILL END OF DATA 
+   foreach ($row as $teloq) 
+   { 
+  ?>
 
   <tr>
-    <td>1</td>
-    <td>Fahmi</td>
-    <td>0126636312</td>
-    <td>Admin</td>
-    <td>Edit | Delete</td>
+  <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
+    <td><?php echo $teloq['id'];?></td>
+    <td><?php echo $teloq['name'];?></td>
+    <td><?php echo $teloq['tel_no'];?></td>
+    <td><?php echo $teloq['role_desc'];?></td>
+    <td align="center"><a href="golfer-update-record.php?id=<?php echo $row["id"];?>">Edit</a> || <a href="golfer-delete-record.php?id=<?php echo $row["id"];?>">Delete</a></td>
   </tr>
-
-  <tr>
-    <td>1</td>
-    <td>Fahmi</td>
-    <td>0126636312</td>
-    <td>Admin</td>
-    <td>Edit | Delete</td>
-  </tr>
-
-  <tr>
-    <td>1</td>
-    <td>Fahmi</td>
-    <td>0126636312</td>
-    <td>Admin</td>
-    <td>Edit | Delete</td>
-  </tr>
-
+  <?php
+    }
+  ?>
 </table>
 
 <?php require_once ('../layouts/footer.php'); ?>
