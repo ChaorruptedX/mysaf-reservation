6d18808250496ffb997fc88f5e3f8f657d6cab39
Asyraf391 <asyrafhanif391@gmail.com>
2022-01-10 09:16:00 +0800

    My Reservation Interface

---
+++ public/user/my-reservation.php
@@ -0,0 +1,43 @@
+<?php require_once ('../layouts/header.php'); userSession(); ?>
+<div>
+    <h1>My Reservation</h1>
+</div>
+
+<?php /* Has Reservation */ ?>
+<div class="wrapper form">
+    <h2> Friday Prayer </h2>
+    <h2> [ 20 January 2020 ] </h2>
+    <table id="user">
+        <tr >
+            <th colspan="2" > RESERVED</th>
+        </tr>
+        
+        <tr>
+            <td><b>Mosque Name:</b></td>
+            <td>Masjid Sayyidina Abu Bakar, UTeM</td>
+        </tr>
+        
+        <tr>
+            <td><b>Reserve by:</b></td>
+            <td>Asyraf Hanif</td>
+        </tr>
+  
+        <tr>
+            <td><b>Reservation Date:</b></td>
+            <td>19 January 2022</td>
+        </tr>
+
+    </table>
+    <br>
+    <div>
+        <a href=""><button class="form back">Cancel Reservation</button></a>
+    </div>
+
+    <?php /* No Reservation */ ?>
+    <div>
+        <h3> No Reservation made </h3>
+    <div>
+        
+</div>
+
+<?php require_once ('../layouts/footer.php'); ?>
