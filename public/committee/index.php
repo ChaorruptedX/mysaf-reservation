9bebeaeca457bec8dbff95bbc7d3e5c87af4a91e
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-13 21:30:00 +0800

    Committee Dashboard - Update System User

--- public/committee/index.php
+++ public/committee/index.php
@@ -1,30 +1,63 @@
 <?php require_once ('../layouts/header.php'); committeeSession(); ?>
+
+<?php
+    try {
+        // Get System User
+        $stmt = $conn->prepare("
+            SELECT
+                SUM(
+                    CASE
+                        WHEN user.id_role = " . lookupRole($conn, "SA001")['id'] . " THEN 1
+                        ELSE 0
+                    END
+                ) AS total_system_administrator,
+                SUM(
+                    CASE
+                        WHEN user.id_role = " . lookupRole($conn, "MC001")['id'] . " THEN 1
+                        ELSE 0
+                    END
+                ) AS total_committee,
+                SUM(
+                    CASE
+                        WHEN user.id_role = " . lookupRole($conn, "U001")['id'] . " THEN 1
+                        ELSE 0
+                    END
+                ) AS total_users
+            FROM user
+            WHERE
+                user.deleted_at='0'
+        ");
+
+        $stmt->execute();
+
+        // Set the Resulting Array to Associative
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $row = $stmt->fetch();
+    }
+    catch (PDOException $e)
+    {
+        dd("Error: " . $e->getMessage());
+    }
+?>
+
 <div>
-<br>
+    <br>
     <h1>Committee Dashboard</h1>
 </div>
+
 <div class="main-part">
     <h2> System User </h2>
     <hr>
-    <div class="box">
-        <div class="icon-part"><br>
-            <img src ="<?= constant("BASEURL") . 'assets/image/user-icon.png' ?>"alt="User Icon" width="50" height="50"/><br>
-            <small>Members</small>
-            <p>985</p>
-        </div>
-        <div class="card-content-part">
-            <a href="#">More Details </a>
-        </div>
-    </div>
 
     <div class="box box-green">
         <div class="icon-part"><br>
         <img src ="<?= constant("BASEURL") . 'assets/image/admin-icon.png' ?>"alt="admin Icon" width="50" height="50"/><br>
                 <small>System Admin</small>
-            <p>2</p>
+            <p><?= $row['total_system_administrator'] ?></p>
         </div>
         <div class="card-content-part">
-            <a href="#">More Details </a>
+            <a href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">More Details </a>
         </div>
     </div>
 
@@ -32,10 +65,21 @@
         <div class="icon-part"><br>
         <img src ="<?= constant("BASEURL") . 'assets/image/committee-icon.jpg' ?>"alt="Committee Icon" width="50" height="50"/><br>
             <small>Mosque Committee</small>
-            <p>11</p>
+            <p><?= $row['total_users'] ?></p>
         </div>
         <div class="card-content-part">
-            <a href="#">More Details </a>
+            <a href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">More Details </a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="icon-part"><br>
+            <img src ="<?= constant("BASEURL") . 'assets/image/user-icon.png' ?>"alt="User Icon" width="50" height="50"/><br>
+            <small>Members</small>
+            <p><?= $row['total_users'] ?></p>
+        </div>
+        <div class="card-content-part">
+            <a href="<?= constant("BASEURL") . 'public/committee/user-management.php' ?>">More Details </a>
         </div>
     </div>
 
@@ -55,5 +99,7 @@
     </div>
     
 </div>
+
 <br>
+
 <?php require_once ('../layouts/footer.php'); ?>
