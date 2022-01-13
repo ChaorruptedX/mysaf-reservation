f7ee8aba3b0d07f936b92b30f3331914d8527ded
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-13 23:22:35 +0800

    Create Saf Reservation - Committee Able to Make Reservation

--- public/committee/create-reservation.php
+++ public/committee/create-reservation.php
@@ -1,5 +1,110 @@
 <?php require_once ('../layouts/header.php'); committeeSession(); ?>
 
+<?php
+    $success_registration_message = false;
+    $reservation_date_validation = true;
+
+    if ($_POST)
+    {
+        $open_datetime = $_POST['open_date'].' '.$_POST['open_time'];
+        $close_datetime = $_POST['close_date'].' '.$_POST['close_time'];
+
+        try {
+            // Check for Existing Reservation Date and Time
+            $stmt = $conn->prepare("
+                SELECT
+                    *
+                FROM reservation
+                WHERE
+                    (
+                        (
+                            reservation.open_time >= '" . $open_datetime . "'
+                            AND reservation.close_time <= '" . $close_datetime . "'
+                        )
+                        OR
+                        (
+                            reservation.open_time <= '" . $open_datetime . "'
+                            AND reservation.close_time >= '" . $close_datetime . "'
+                        )
+                        OR
+                        (
+                            reservation.open_time >= '" . $open_datetime . "'
+                            AND reservation.open_time <= '" . $close_datetime . "'
+                        )
+                        OR
+                        (
+                            reservation.close_time >= '" . $open_datetime . "'
+                            AND reservation.close_time <= '" . $close_datetime . "'
+                        )
+                    )
+                    AND reservation.deleted_at = '0'
+            ");
+    
+            $stmt->execute();
+    
+            // Set the Resulting Array to Associative
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    
+            $row = $stmt->fetch();
+        }
+        catch (PDOException $e)
+        {
+            dd("Error: " . $e->getMessage());
+        }
+
+        if (empty($row))
+        {
+            try // Register Reservation
+            {
+                // Prepare SQL and Bind Parameters
+                $stmt = $conn->prepare("
+                    INSERT INTO reservation (
+                        id_personal_detail,
+                        id_mosque,
+                        name,
+                        open_time,
+                        close_time,
+                        maximum_capacity
+                    )
+                    VALUES (
+                        :id_personal_detail,
+                        :id_mosque,
+                        :name,
+                        :open_time,
+                        :close_time,
+                        :maximum_capacity
+                    )
+                ");
+                $stmt->bindParam(':id_personal_detail', $id_personal_detail);
+                $stmt->bindParam(':id_mosque', $id_mosque);
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':open_time', $open_time);
+                $stmt->bindParam(':close_time', $close_time);
+                $stmt->bindParam(':maximum_capacity', $maximum_capacity);
+
+                // Insert a Row
+                $id_personal_detail = getPersonalDetailbyIdUser($conn, $_SESSION['id_user'])['id'];
+                $id_mosque = getActiveMosque($conn)['id'];
+                $name = $_POST['name'];
+                $open_time = $open_datetime;
+                $close_time = $close_datetime;
+                $maximum_capacity = $_POST['capacity'];
+                $stmt->execute();
+
+                $success_registration_message = true;
+            }
+            catch (PDOException $e)
+            {
+                dd("Error: " . $e->getMessage());
+            }
+        }
+        else
+        {
+            $reservation_date_validation = false;
+        }
+    }
+?>
+
 <div>
     <br>
     <h1>Create Saf Reservation</h1>
@@ -8,28 +113,39 @@
 <link rel ="stylesheet" type="text/css" href="<?= constant("BASEURL") . 'assets/css/reservation.css' ?>">
 
 <div class="wrapper form">
-    <form>
-        <label class="form" for="date">Prayer Date</label>
-        <input class="input form" type="date" name="date">
-        
+    <form method="post">
+        <?php if ($success_registration_message === true) : ?>
+            <div style="text-align: center; margin-bottom: 15px">
+                <label class="validation-success">Reservation has been successfully registered into the system.</label>
+            </div>
+        <?php endif; ?>
+        <?php if ($reservation_date_validation === false) : ?>
+            <div style="text-align: center; margin-bottom: 15px">
+                <label class="validation-error">The reservation date and time clash with another reservation.</label>
+            </div>
+        <?php endif; ?>
+
+        <!-- <label class="form" for="date">Prayer Date</label>
+        <input class="input form" type="date" name="date"> -->
+
+        <label class="form" for="name">Name</label>
+        <input type="text" name="name" class="input form" required>
+
+        <label class="form" for="open_time">Open Date & Time</label>
+        <input class="input form" type="date" name="open_date" required>
+        <input class="input form" type="time" name="open_time" required>
+
+        <label class="form" for="close_time">Close Date & Time</label>
+        <input class="input form" type="date" name="close_date" required>
+        <input class="input form" type="time" name="close_time" required>
+
         <label class="form" for="capacity">Set Capcity</label>
-        <select name="capacity">
-            <option value="50">50</option>
-            <option value="100">100</option>
-            <option value="150">150</option>
-            <option value="200">200</option>
-        </select>
-
-        <label class="form" for="phone">Reservation Open Day&Date</label>
-        <input class="input form" type="date" name="open_date"> 
-        <input class="input form" type="time" name="open_time"> 
-
-        <label class="form" for="phone">Reservation Close Day&Date</label>
-        <input class="input form" type="date" name="close_date"> 
-        <input class="input form" type="time" name="close_time"> 
+        <input class="input form" type="number" name="capacity" required>
 
         <button class="form submit">Create Reservation</button>
-  </form>
+    </form>
 </div>
+
 <br>
+
 <?php require_once ('../layouts/footer.php'); ?>
