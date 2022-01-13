be2b2362852099b53abff77ea28e48586a0a2f80
Asyraf391 <asyrafhanif391@gmail.com>
2022-01-13 08:28:43 +0800

    User My account nav

--- public/layouts/navigation/nav-user.php
+++ public/layouts/navigation/nav-user.php
@@ -3,7 +3,7 @@
         <a class="<?= active_url(["b031910250", "mysaf-reservation", "public", "user", "index.php"]); ?>" href="<?= constant("BASEURL") . 'public/user/index.php' ?>">Home</a>
         <a class="<?= active_url(["my-reservation.php"]); ?>"href="<?= constant("BASEURL") . 'public/user/my-reservation.php' ?>">My Reservation</a>
         <a href="#history">History</a>
-        <a href="#my-account">My Account</a>
+        <a class="<?= active_url(["my-user-account.php"]); ?>"href="<?= constant("BASEURL") . 'public/user/my-user-account.php' ?>">My Account</a>
         <a class="<?= active_url("logout.php"); ?>" href="<?= constant("BASEURL") . 'public/logout.php' ?>" style="float: right;">Logout</a>
     </div>
 </nav>
