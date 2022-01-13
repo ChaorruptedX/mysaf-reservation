82c58748ef8e18e7c41eff992360ab00a6ced417
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-14 01:46:30 +0800

    User Dashboard - Update Display Data

--- public/user/index.php
+++ public/user/index.php
@@ -1,26 +1,45 @@
 <?php require_once ('../layouts/header.php'); userSession(); ?>
+
+<?php
+    $reservation = getClosestReservation($conn);
+?>
+
 <div>
+
 <br>
     <h1>User Dashboard</h1>
 </div>
+
 <div class="main-part">
-    <h2>Saf Reservation</h2>
-    <hr>
-    <h3> Saf Resrvation for Friday Prayer on [ 20 January 2022 ] </h3>
-    <div class="box-saf">
-        <div class="icon-part"><br>
-            <img src ="<?= constant("BASEURL") . 'assets/image/saf-icon.png' ?>"alt="User Icon" width="50" height="50"/><br>
-            <h3> Open - Thursday [19 January 2022] 10:00 AM</h3>
-            <h3> Close - Friday [20 January 2022] 10:00 AM</h3>
-            <h4>Total Reservation</h4>
-            <p>130 / 200</p>
+    <?php if (!empty($reservation)) : ?>
+
+        <h2>Saf Reservation</h2>
+
+        <hr>
+
+        <h3> Resrvation for <?= $reservation['name']; ?> on <?= getDateFormat($reservation['open_time']); ?> </h3>
+
+        <div class="box-saf">
+            
+            <div class="icon-part"><br>
+                <img src ="<?= constant("BASEURL") . 'assets/image/saf-icon.png' ?>"alt="User Icon" width="50" height="50"/><br>
+                <h3> Open - <?= getFullDateTimeFormat($reservation['open_time']); ?></h3>
+                <h3> Close - <?= getFullDateTimeFormat($reservation['close_time']); ?></h3>
+                <h4>Total Reservation</h4>
+                <p><?= $reservation['total_user_reserved']; ?> / <?= $reservation['maximum_capacity'] ?></p>
+            </div>
+            
+            <div>
+                <a href="<?= constant("BASEURL")?>"><button class="button home-reserve">Reserve Now</button></a>
+            </div>
+
+            <br>
+
         </div>
-        
-        <div>
-            <a href="<?= constant("BASEURL")?>"><button class="button home-reserve">Reserve Now</button></a>
-        </div>
-        <br>
-    </div>
+
+    <?php endif; ?>
 </div>
+
 <br>
+
 <?php require_once ('../layouts/footer.php'); ?>
