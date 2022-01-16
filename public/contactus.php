89e513ccc1068feb19ab60acbe96e0e6382a54fc
abu <abu@gmail.com>
2022-01-16 15:33:36 +0800

    a

--- public/contactus.php
+++ public/contactus.php
@@ -6,9 +6,6 @@
         <h2 name = "title">Contact Us</h2>
         <p>For all enquires,please leave us a message: </p>
             <div class="column">
-                <img src="<?= constant("BASEURL") . 'assets/image/masjid.jpg' ?>" style="width:100%">
-            </div>
-            <div class="column">
                 <form action="#">
                     <span class ="label">First Name</span>
                         <input type="text" id="fname" name="firstname" placeholder="Your name..">
@@ -23,7 +20,7 @@
                             <option value="Negeri">Negeri Sembilan</option>
                             <option value="Penang">Penang</option>
                         </select>
-                    <span class ="label">Subject</span>
+                    <span class ="label"></span>
                     <textarea id="subject" name="subject" placeholder="Write something.." style="height:160px"></textarea>
                     <input type="submit" value="Submit">
                 </form>
@@ -31,9 +28,9 @@
         <div class="center">
             <h2> OR </h2><br><br>
             <h3>Connect with us through our social media ! </h3>
-            <p><a href="mailto:abu@gmail.com" target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/gmail.png' ?>" width="60px" height="60px">   </a>
-            <a href="https://twitter.com/i/flow/login"target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/twitter.png' ?>" width="60px" height="60px">   </a>
-            <a href="https://www.facebook.com/"target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/fb.png' ?>" width="60px" height="60px"></a>   </p>    
+            <p><a href="mailto:abu@gmail.com" target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/gmail.png' ?>" width="60px" height="60px"></a>
+            <a href="https://twitter.com/i/flow/login"target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/twitter.png' ?>" width="60px" height="60px"></a>
+            <a href="https://www.facebook.com/"target="_blank"><img src="<?= constant("BASEURL") . 'assets/image/fb.png' ?>" width="60px" height="60px"></a></p>    
         </div>   
     </div>
 
