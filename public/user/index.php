8bbe07d5c7f5fbeba74c541382a2e87a16c6f1f2
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-18 02:06:06 +0800

    User Dashboard - User Able to Make Reservation

--- public/user/index.php
+++ public/user/index.php
@@ -1,7 +1,54 @@
 <?php require_once ('../layouts/header.php'); userSession(); ?>
 
 <?php
+    $capacity_validation = true;
+    $success_reserved_message = false;
     $reservation = getClosestReservation($conn);
+    
+    if ($_POST)
+    {
+        if ($reservation['total_user_reserved'] < $reservation['maximum_capacity']) // If There Is Empty Slot, Proceed
+        {
+            try // Make Reservation
+                {  
+                    // Prepare SQL and Bind Parameters
+                    $stmt = $conn->prepare("
+                        INSERT INTO user_reservation (
+                            id_personal_detail,
+                            id_reservation,
+                            status
+                        )
+                        VALUES (
+                            :id_personal_detail,
+                            :id_reservation,
+                            :status
+                        )
+                    ");
+                    $stmt->bindParam(':id_personal_detail', $id_personal_detail);
+                    $stmt->bindParam(':id_reservation', $id_reservation);
+                    $stmt->bindParam(':status', $status);
+
+                    // Insert a Row
+                    $id_personal_detail = getPersonalDetailbyIdUser($conn, $_SESSION['id_user'])['id'];
+                    $id_reservation = $_POST['id_reservation'];
+                    $status = 1; // Reserved
+                    $stmt->execute();
+
+                    $success_reserved_message = true;
+                    $reservation = getClosestReservation($conn);
+                }
+                catch (PDOException $e)
+                {
+                    dd("Error: " . $e->getMessage());
+                }
+        }
+        else
+        {
+            $capacity_validation = false;
+        }
+    }
+
+    $user_reservation_exist = checkExistingUserReservation($conn, getPersonalDetailbyIdUser($conn, $_SESSION['id_user'])['id'], $reservation['id']);
 ?>
 
 <div>
@@ -30,7 +77,14 @@
             </div>
             
             <div>
-                <a href="<?= constant("BASEURL")?>"><button class="button home-reserve">Reserve Now</button></a>
+                <?php if (!$user_reservation_exist) : ?>
+                <form id="form-reserve" method="post">
+                    <input type="hidden" name="id_reservation" value="<?= $reservation['id']; ?>">
+                    <button id="reserve-now" type="submit" class="button home-reserve">Reserve Now</button>
+                </form>
+                <?php else : ?>
+                    <button class="button home-reserved">Reserved</button>
+                <?php endif; ?>
             </div>
 
             <br>
@@ -41,5 +95,53 @@
 </div>
 
 <br>
+
+<script type="text/javascript">
+
+$(function() { // Shorthand for $( document ).ready()
+
+    let capacity_validation = '<?= json_encode($capacity_validation); ?>';
+    let success_reserved_message = '<?= json_encode($success_reserved_message); ?>';
+    
+    if (capacity_validation == "false")
+    {
+        Swal.fire({
+            icon: 'error',
+            title: 'Reservation Failed',
+            text: 'Maximum capacity has been reached.',
+        });
+    }
+
+    if (success_reserved_message == "true")
+    {
+        Swal.fire({
+            icon: 'success',
+            title: 'Reservation Success',
+            text: 'You have successfully made a reservation.',
+        });
+    }
+
+    $(document).on("click", "button#reserve-now", function(event) {
+
+        event.preventDefault();
+
+        Swal.fire({
+            title: 'Make Reservation',
+            text: "Are you sure to reserve?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+        }).then((result) => {
+            if (result.isConfirmed)
+                $("form#form-reserve").submit();
+        })
+
+    });
+
+});
+
+</script>
 
 <?php require_once ('../layouts/footer.php'); ?>
