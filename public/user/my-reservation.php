9c61e38edc2ce881c81625f6e14d6e8f50f6976b
ChaorruptedX <ChaorruptedX@gmail.com>
2022-01-18 10:17:02 +0800

    My Reservation - Display Reservation Data

--- public/user/my-reservation.php
+++ public/user/my-reservation.php
@@ -1,5 +1,11 @@
 <?php require_once ('../layouts/header.php'); userSession(); ?>
 
+<?php
+    $mosque = getActiveMosque($conn);
+    $reservation = getClosestReservation($conn);
+    $user_reservation_exist = checkExistingUserReservation($conn, getPersonalDetailbyIdUser($conn, $_SESSION['id_user'])['id'], $reservation['id']);
+?>
+
 <br>
 <div>
     <h1>My Reservation</h1>
@@ -7,38 +13,38 @@
 
 <?php /* Has Reservation */ ?>
 <div class="wrapper form">
-    <h2> Friday Prayer </h2>
-    <h2> [ 20 January 2020 ] </h2>
-    <table id="user">
-        <tr >
-            <th colspan="2" > RESERVED</th>
-        </tr>
-        
-        <tr>
-            <td><b>Mosque Name:</b></td>
-            <td>Masjid Sayyidina Abu Bakar, UTeM</td>
-        </tr>
-        
-        <tr>
-            <td><b>Reserve by:</b></td>
-            <td>Asyraf Hanif</td>
-        </tr>
-  
-        <tr>
-            <td><b>Reservation Date:</b></td>
-            <td>19 January 2022</td>
-        </tr>
 
-    </table>
-    <br>
-    <div>
-        <a href=""><button class="form back">Cancel Reservation</button></a>
-    </div>
+    <?php if ($user_reservation_exist) : ?>
 
-    <?php /* No Reservation */ ?>
-    <div>
-        <h3> No Reservation made </h3>
-    <div>
+        <h2> <?= $reservation['name']; ?> </h2>
+        <table id="user">
+            <tr >
+                <th colspan="2">RESERVED</th>
+            </tr>
+            
+            <tr>
+                <td><b>Mosque Name:</b></td>
+                <td><?= $mosque['name']; ?></td>
+            </tr>
+    
+            <tr>
+                <td><b>Reservation Date:</b></td>
+                <td><?= getFullDateTimeFormat($reservation['open_time']); ?> - <?= getFullDateTimeFormat($reservation['close_time']); ?></td>
+            </tr>
+
+        </table>
+        <br>
+        <div>
+            <a href=""><button class="form back">Cancel Reservation</button></a>
+        </div>
+
+    <?php else : ?>
+
+        <div>
+            <h3> No Reservation Has Been Made </h3>
+        </div>
+
+    <?php endif; ?>
         
 </div>
 
